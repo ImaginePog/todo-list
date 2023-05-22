e6ca744da1d835822476a6be90a4ddb21e8e1840
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-05-22 22:53:49 +0545

    Change status to a boolean

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,14 +1,19 @@
 export default class Task {
 	#creationDate;
+	#completed;
 
 	constructor(taskInfo) {
 		this.name = taskInfo.name;
 		this.#creationDate = new Date();
 		this.dueDate = taskInfo.dueDate;
-		this.status = incomplete;
+		this.#completed = false;
 		this.priority = taskInfo.priority;
 		this.description = taskInfo.description;
 		this.starred = taskInfo.starred;
+	}
+
+	complete() {
+		this.#completed = true;
 	}
 
 	get creationDate() {
