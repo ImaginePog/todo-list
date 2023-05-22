d8685d528f918b8e1f02a96a3ff2a101af80c052
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-05-23 00:18:11 +0545

    Add features for task modal

--- src/modules/DisplayController.js
+++ src/modules/DisplayController.js
@@ -2,6 +2,8 @@ import ProjectManager from "./ProjectManager";
 
 const DisplayController = (() => {
 	const projectList = document.querySelector(".project-list");
+	const taskModal = document.querySelector(".task-modal");
+	const projectSelect = document.querySelector("#project-select");
 
 	function createProjectTab(projectName) {
 		const li = document.createElement("li");
@@ -25,15 +27,48 @@ const DisplayController = (() => {
 		console.log(projNames);
 		const frag = document.createDocumentFragment();
 
-		projNames.forEach((proj) => {
-			const tab = createProjectTab(proj);
+		for (let i = 1; i < projNames.length; ++i) {
+			const tab = createProjectTab(projNames[i]);
 			frag.append(tab);
-		});
+		}
 
 		projectList.append(frag);
 	}
 
-	return { renderProjectTabs };
+	function createProjectOption(proj) {
+		const opt = document.createElement("option");
+		opt.textContent = proj;
+
+		return opt;
+	}
+
+	//Renders all the current projects for selection
+	function renderProjectSelect() {
+		projectSelect.textContent = "";
+
+		const projNames = ProjectManager.getAllProjectNames();
+		const frag = document.createDocumentFragment();
+
+		for (let i = 0; i < projNames.length; ++i) {
+			const opt = createProjectOption(projNames[i]);
+			opt.value = i;
+
+			frag.append(opt);
+		}
+
+		projectSelect.append(frag);
+	}
+
+	function openTaskModal() {
+		taskModal.classList.remove("hide");
+		renderProjectSelect();
+	}
+
+	function closeTaskModal() {
+		taskModal.classList.add("hide");
+	}
+
+	return { renderProjectTabs, openTaskModal, closeTaskModal };
 })();
 
 export default DisplayController;
