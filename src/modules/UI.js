c98d4826017eb01586a62ccb66b45987a3c258ab
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-05-23 00:18:56 +0545

    UI module handles events and interface

---
+++ src/modules/UI.js
@@ -0,0 +1,69 @@
+import DisplayController from "./DisplayController";
+import ProjectManager from "./ProjectManager";
+
+const UI = (() => {
+	const nav = document.querySelector(".nav");
+	const main = document.querySelector("#main");
+	const taskForm = document.querySelector(".add-task-form");
+
+	function handleMainClick(e) {
+		if (e.target.dataset.action === "openTaskModal") {
+			DisplayController.openTaskModal();
+		}
+	}
+
+	function handleNavClick(e) {
+		const navId = e.target.dataset.navId;
+
+		if (!navId) {
+			return;
+		}
+
+		switch (navId) {
+			case "inbox":
+				console.log("display inbox");
+				break;
+			case "today":
+				console.log("display today");
+				break;
+			case "projects":
+				console.log("display projects");
+				break;
+			default:
+				console.log(`display ${navId} project`);
+		}
+	}
+
+	function handleTaskFormClick(e) {
+		console.log(e + "lol");
+	}
+
+	function handleTaskFormSubmit(e) {
+		e.preventDefault();
+
+		const form = e.currentTarget;
+
+		let taskInfo = {
+			name: form.elements.title.value,
+			description: form.elements.description.value,
+			dueDate: form.elements.dueDate.value,
+			priority: form.elements.priority.value,
+		};
+
+		let projectId = form.elements.project.value;
+
+		ProjectManager.addTaskToProject(projectId, taskInfo);
+
+		form.reset();
+		DisplayController.closeTaskModal();
+	}
+
+	main.addEventListener("click", handleMainClick);
+	nav.addEventListener("click", handleNavClick);
+	taskForm.addEventListener("click", handleTaskFormClick);
+	taskForm.addEventListener("submit", handleTaskFormSubmit);
+
+	return {};
+})();
+
+export default UI;
