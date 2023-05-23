ed268de63df584b2a7a1dc2eeb7260d0bcb1c60d
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-05-23 18:34:14 +0545

    Change rendering using currentTab

--- src/modules/DisplayController.js
+++ src/modules/DisplayController.js
@@ -3,7 +3,9 @@ import ProjectManager from "./ProjectManager";
 const DisplayController = (() => {
 	const projectList = document.querySelector(".project-list");
 	const taskModal = document.querySelector(".task-modal");
-	const projectSelect = document.querySelector("#project-select");
+	const dynamicDisplay = document.querySelector(".dynamic-content");
+
+	let currentTab;
 
 	function createProjectTab(projectName) {
 		const li = document.createElement("li");
@@ -38,15 +40,20 @@ const DisplayController = (() => {
 	function createProjectOption(proj) {
 		const opt = document.createElement("option");
 		opt.textContent = proj;
+		const currProj = ProjectManager.getCurrentProject().name;
+		if (currProj === proj) {
+			opt.selected = true;
+		}
 
 		return opt;
 	}
 
 	//Renders all the current projects for selection
-	function renderProjectSelect() {
-		projectSelect.textContent = "";
+	function renderProjectSelect(taskForm) {
+		taskForm.elements.project.textContent = "";
 
 		const projNames = ProjectManager.getAllProjectNames();
+
 		const frag = document.createDocumentFragment();
 
 		for (let i = 0; i < projNames.length; ++i) {
@@ -56,19 +63,120 @@ const DisplayController = (() => {
 			frag.append(opt);
 		}
 
-		projectSelect.append(frag);
+		taskForm.elements.project.append(frag);
 	}
 
-	function openTaskModal() {
+	function clearDynamicDisplay() {
+		dynamicDisplay.textContent = "";
+	}
+
+	function createDisplayTitle(title) {
+		const displayTitle = document.createElement("h2");
+		displayTitle.textContent = title;
+		displayTitle.classList.add("display-title");
+
+		return displayTitle;
+	}
+
+	function createTaskContainers(proj) {
+		const frag = document.createDocumentFragment();
+		for (let i = 0; i < proj.tasks.length; ++i) {
+			const taskContainer = document.createElement("li");
+			taskContainer.classList.add("task-container");
+
+			const title = document.createElement("h3");
+			title.classList.add("task-title");
+			title.textContent = proj.tasks[i].name;
+
+			taskContainer.append(title);
+			frag.append(taskContainer);
+		}
+
+		return frag;
+	}
+
+	function createTaskList() {
+		const list = document.createElement("ul");
+		list.classList.add("task-list");
+
+		return list;
+	}
+
+	function renderCurrentProject() {
+		clearDynamicDisplay();
+		const currProject = ProjectManager.getCurrentProject();
+		const title = createDisplayTitle(currProject.name);
+
+		const taskList = createTaskList();
+
+		const tasks = createTaskContainers(currProject);
+		taskList.append(tasks);
+
+		dynamicDisplay.append(title, taskList);
+	}
+
+	function openTaskModal(taskForm) {
 		taskModal.classList.remove("hide");
-		renderProjectSelect();
+		renderProjectSelect(taskForm);
+
+		if (currentTab === "today") {
+			taskForm.elements.dueDate.valueAsDate = new Date();
+		}
 	}
 
 	function closeTaskModal() {
 		taskModal.classList.add("hide");
 	}
 
-	return { renderProjectTabs, openTaskModal, closeTaskModal };
+	function renderToday() {
+		clearDynamicDisplay();
+
+		const title = createDisplayTitle("Today");
+
+		const filteredProjs = ProjectManager.getProjectsFiltered("today");
+		console.log(filteredProjs);
+
+		const displayList = document.createElement("ul");
+
+		filteredProjs.forEach((proj) => {
+			const tasks = createTaskContainers(proj);
+			displayList.append(tasks);
+		});
+
+		dynamicDisplay.append(title, displayList);
+	}
+
+	function renderCurrentTab() {
+		clearDynamicDisplay();
+
+		switch (currentTab) {
+			case "project":
+				renderCurrentProject();
+				break;
+			case "today":
+				renderToday();
+				break;
+			case "projects":
+				//renderprojects
+				break;
+			default:
+			//handle default
+		}
+	}
+
+	function changeCurrentTab(tab) {
+		currentTab = tab;
+		renderCurrentTab();
+	}
+
+	return {
+		renderProjectTabs,
+		openTaskModal,
+		closeTaskModal,
+		renderToday,
+		changeCurrentTab,
+		renderCurrentTab,
+	};
 })();
 
 export default DisplayController;
