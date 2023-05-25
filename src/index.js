0b764100a7d90da6f78f1d6943fd8bf070e71c46
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-05-25 12:34:06 +0545

    Makeshift entrypoint for testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("hello world");
+import DisplayController from "./modules/DisplayController";
+import ProjectManager from "./modules/ProjectManager";
+import UI from "./modules/UI";
+import "./styles/index.css";
+
+ProjectManager.addProject("Inbox");
+
+const inTab = document.querySelector('button[data-action="inbox"]');
+inTab.click();
+
+DisplayController.renderProjectTabs();
