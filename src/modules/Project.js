89f473317926c19ec068d3229e6e3e45eb80344a
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-09-17 01:57:55 +0545

    Make tasks public
    JSON parsing doesnt seem to work for private
    variables, get being a function (probably? :P)

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -2,26 +2,24 @@ import { isToday } from "date-fns";
 import Task from "./Task";
 
 export default class Project {
-  #tasks;
-
   constructor(projName, id) {
     this.name = projName;
-    this.#tasks = [];
+    this.tasks = [];
     this.id = id;
   }
 
   addTask(taskInfo) {
-    this.#tasks.push(new Task(taskInfo, this.#tasks.length, this.id));
+    this.tasks.push(new Task(taskInfo, this.tasks.length, this.id));
   }
 
   removeTask(taskId) {
-    if (taskId >= 0 && taskId < this.#tasks.length) {
-      this.#tasks.splice(taskId, 1);
+    if (taskId >= 0 && taskId < this.tasks.length) {
+      this.tasks.splice(taskId, 1);
     }
   }
 
   editTask(taskId, newTaskInfo) {
-    this.#tasks[taskId] = new Task(newTaskInfo, taskId, this.id);
+    this.tasks[taskId] = new Task(newTaskInfo, taskId, this.id);
   }
 
   getTasksDueToday() {
@@ -32,18 +30,14 @@ export default class Project {
   }
 
   getIncompleteTasks() {
-    return this.#tasks.filter((task) => !task.completed);
+    return this.tasks.filter((task) => !task.completed);
   }
 
   getCompleteTasks() {
-    return this.#tasks.filter((task) => task.completed);
+    return this.tasks.filter((task) => task.completed);
   }
 
   toggleTaskStatus(taskId) {
-    this.#tasks[taskId].toggleStatus();
-  }
-
-  get tasks() {
-    return this.#tasks;
+    this.tasks[taskId].toggleStatus();
   }
 }
