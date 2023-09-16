060fb8178914ce311d170540af0bad5544ff56b5
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-09-16 18:15:43 +0545

    Remove files
    Bruv nothing works coming back after few months
    basically tryna restart :/

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -2,48 +2,48 @@ import { isToday } from "date-fns";
 import Task from "./Task";
 
 export default class Project {
-	#tasks;
+  #tasks;
 
-	constructor(projName, id) {
-		this.name = projName;
-		this.#tasks = [];
-		this.id = id;
-	}
+  constructor(projName, id) {
+    this.name = projName;
+    this.#tasks = [];
+    this.id = id;
+  }
 
-	addTask(taskInfo) {
-		this.#tasks.push(new Task(taskInfo, this.#tasks.length, this.id));
-	}
+  addTask(taskInfo) {
+    this.#tasks.push(new Task(taskInfo, this.#tasks.length, this.id));
+  }
 
-	removeTask(taskId) {
-		if (taskId >= 0 && taskId < this.#tasks.length) {
-			this.#tasks.splice(taskId, 1);
-		}
-	}
+  removeTask(taskId) {
+    if (taskId >= 0 && taskId < this.#tasks.length) {
+      this.#tasks.splice(taskId, 1);
+    }
+  }
 
-	editTask(taskId, newTaskInfo) {
-		this.#tasks[taskId] = new Task(newTaskInfo, taskId, this.id);
-	}
+  editTask(taskId, newTaskInfo) {
+    this.#tasks[taskId] = new Task(newTaskInfo, taskId, this.id);
+  }
 
-	getTasksDueToday() {
-		const tasks = this.getIncompleteTasks();
-		return tasks.filter((task) => {
-			if (task.dueDate) return isToday(task.dueDate);
-		});
-	}
+  getTasksDueToday() {
+    const tasks = this.getIncompleteTasks();
+    return tasks.filter((task) => {
+      if (task.dueDate) return isToday(task.dueDate);
+    });
+  }
 
-	getIncompleteTasks() {
-		return this.#tasks.filter((task) => !task.completed);
-	}
+  getIncompleteTasks() {
+    return this.#tasks.filter((task) => !task.completed);
+  }
 
-	getCompleteTasks() {
-		return this.#tasks.filter((task) => task.completed);
-	}
+  getCompleteTasks() {
+    return this.#tasks.filter((task) => task.completed);
+  }
 
-	toggleTaskStatus(taskId) {
-		this.#tasks[taskId].toggleStatus();
-	}
+  toggleTaskStatus(taskId) {
+    this.#tasks[taskId].toggleStatus();
+  }
 
-	get tasks() {
-		return this.#tasks;
-	}
+  get tasks() {
+    return this.#tasks;
+  }
 }
--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,53 +1,53 @@
 import { isAfter, parseISO } from "date-fns";
 
 export default class Task {
-	#creationDate;
-	#completed;
+  #creationDate;
+  #completed;
 
-	constructor(taskInfo, id, projectId) {
-		this.name = taskInfo.name;
-		this.#creationDate = new Date();
+  constructor(taskInfo, id, projectId) {
+    this.name = taskInfo.name;
+    this.#creationDate = new Date();
 
-		if (taskInfo.dueDate) {
-			taskInfo.dueDate = parseISO(taskInfo.dueDate);
+    if (taskInfo.dueDate) {
+      taskInfo.dueDate = parseISO(taskInfo.dueDate);
 
-			let now = new Date();
-			now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let now = new Date();
+      now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
-			this.overdue = isAfter(now, taskInfo.dueDate);
-			this.dueDate = taskInfo.dueDate;
-		} else {
-			this.overdue = false;
-			this.dueDate = "";
-		}
+      this.overdue = isAfter(now, taskInfo.dueDate);
+      this.dueDate = taskInfo.dueDate;
+    } else {
+      this.overdue = false;
+      this.dueDate = "";
+    }
 
-		this.#completed = false;
-		this.priority = taskInfo.priority;
-		this.description = taskInfo.description;
-		this.starred = taskInfo.starred;
-		this.projectId = projectId;
-		this.id = id;
-	}
+    this.#completed = false;
+    this.priority = taskInfo.priority;
+    this.description = taskInfo.description;
+    this.starred = taskInfo.starred;
+    this.projectId = projectId;
+    this.id = id;
+  }
 
-	toggleStatus() {
-		if (this.#completed) {
-			this.#completed = false;
+  toggleStatus() {
+    if (this.#completed) {
+      this.#completed = false;
 
-			let now = new Date();
-			now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+      let now = new Date();
+      now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
-			this.overdue = isAfter(now, parseISO(this.dueDate));
-		} else {
-			this.#completed = true;
-			this.overdue = false;
-		}
-	}
+      this.overdue = isAfter(now, parseISO(this.dueDate));
+    } else {
+      this.#completed = true;
+      this.overdue = false;
+    }
+  }
 
-	get completed() {
-		return this.#completed;
-	}
+  get completed() {
+    return this.#completed;
+  }
 
-	get creationDate() {
-		return this.#creationDate;
-	}
+  get creationDate() {
+    return this.#creationDate;
+  }
 }
