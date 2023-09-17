71f5284d9261e28d09d9634dabc1049ade0a6e61
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-09-17 20:16:58 +0545

    Create Storage module
    Interface to store and get data from local storage

---
+++ src/modules/Storage.js
@@ -0,0 +1,23 @@
+const Storage = (() => {
+  //Updates the state stored in the localStorage
+  //If the state is empty removes the key from localStorage
+  function updateStorage(state) {
+    localStorage.setItem("state", JSON.stringify(state));
+
+    if (JSON.parse(localStorage.getItem("state")).length == 0) {
+      localStorage.removeItem("state");
+    }
+  }
+
+  //Returns the parsed state from the storage if it exists, returns false otherwise
+  function getStoredState() {
+    if (!localStorage.getItem("state")) {
+      return false;
+    }
+
+    const storedState = JSON.parse(localStorage.getItem("state"));
+    return storedState;
+  }
+
+  return { updateStorage, getStoredState };
+})();
