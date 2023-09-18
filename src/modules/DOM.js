0d295073dc5fc9b7947978c2417d5d23e2394a12
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-09-18 14:42:13 +0545

    Create utility function for creating new elements

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,14 +1,18 @@
 const DOM = (() => {
   let objects = {};
 
+  //Private function of the module that queries the DOM
+  //Queries 'document' object by default
   function queryObject(query, parent = document) {
     objects[query] = parent.querySelector(query);
     console.log(objects);
   }
 
+  //Loads all the objects on the creation of DOM module
   function loadObjects() {
     queryObject("#main");
     queryObject("#aside");
+    queryObject("#header"); //temp
     queryObject(".sidebar-project-list", objects["#aside"]);
     queryObject(".add-task-form");
     queryObject("#project-select", objects["#.add-task-form"]);
@@ -16,6 +20,10 @@ const DOM = (() => {
     queryObject(".edit-task-form");
   }
 
+  // Returns objects that are already loaded based on the query
+  // If the object doesnt exist then shit... its a dumb mistake probably
+  // maybe add loading the document if the object is not loaded
+  // dont wanna tho
   function getObject(query) {
     if (!objects[query]) {
       console.error("FATAL EROOR WE AINT DEALING WITH THIS SHIT NAH");
@@ -25,8 +33,42 @@ const DOM = (() => {
     return objects[query];
   }
 
+  //Returns an element created based on the elementName and properties
+  //properties object is handled based on the properites provided and
+  //added to the element
+  function createElement(elementName, properties) {
+    const element = document.createElement(elementName);
+
+    if (properties["innerText"]) {
+      element.innerText = properties["innerText"];
+    }
+
+    if (properties["classList"]) {
+      properties["classList"].forEach((className) => {
+        element.classList.add(className);
+      });
+    }
+
+    if (properties["attributes"]) {
+      for (const [property, value] of Object.entries(
+        properties["attributes"]
+      )) {
+        element.setAttribute(property, value);
+      }
+    }
+    objects["#header"].append(element);
+
+    if (properties["dataset"]) {
+      for (const [name, value] of Object.entries(properties["dataset"])) {
+        element.dataset[name] = value;
+      }
+    }
+
+    return element;
+  }
+
   loadObjects();
-  return { getObject };
+  return { getObject, createElement };
 })();
 
 export default DOM;
